a4380718bd987e4eb805e1c28a8831030013cd71
YogeshWEB <neemaker246@gmail.com>
2024-02-24 01:03:38 +0530

    image

--- Netflix-Project-Basic-UI-With-api-handling/src/App.jsx
+++ Netflix-Project-Basic-UI-With-api-handling/src/App.jsx
@@ -4,10 +4,10 @@ import Data from "./Data"
 function App() {
   // console.log(Data)
   return (
-    <>
-      <div className="flex justify-center text-4xl font-bold">Hello</div>
+    <div className="bg-[#1c1c1c]">
+      <div className="flex justify-center text-4xl font-bold text-red-500">NETFLEX</div>
       <Data />
-    </>
+    </div>
   )
 }
 
--- Netflix-Project-Basic-UI-With-api-handling/src/Data.jsx
+++ Netflix-Project-Basic-UI-With-api-handling/src/Data.jsx
@@ -3,28 +3,42 @@ import { useEffect, useState } from "react";
 
 
 const key = "Bearer "+import.meta.env.REACT_APP_API_ACCESS_TOKEN;
-
+const baseURL = "http://image.tmdb.org/t/p/w300";
 
 function Data() {
- const [posts,setPosts]=useState([])
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
-       .then((response) => response.json())
-       .then((data) => {
-          console.log(data);
-          setPosts(data);
-       })
-       .catch((err) => {
-          console.log(err.message);
-       });
- }, []);
+ const [result,setResult]=useState([])
+ const [page,setPage] = useState(1);
+ const options = {
+   method: 'GET',
+   headers: {
+     accept: 'application/json',
+     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMzc0Y2NiOTA1YzQyOTcyZmU0ZDBhZDA4MTNlYzk1NyIsInN1YiI6IjY1YThmZDE2ZWEzOTQ5MDEzMTFmNDYwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7QYyy6-wCjsEt4gd9pYLfFJcaMw6wBHvIgvIEVHj2rk'
+   }
+ };
+ 
 
+  useEffect(() => {
+   fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`, options)
+   .then(response => response.json())
+   .then(response => setResult(response.results))
+   .catch(err => console.error(err));
+ }, [page]);
+ const pageinc = () =>{
+   setPage(page=>page+1);
+ }
   return (
-    <div >             
-          {posts.map((val)=>{
-           return  <li key={val.id}>{val.body}</li>
+   <>
+   <div 
+   className="flex m-0 bg-red-500 rounded-md size-12 justify-center items-center font-normal text-white"
+   onClick={pageinc}>Next</div>
+    <div className="flex flex-wrap justify-center items-center">             
+          {result.map((val)=>{
+           return  (<div key={val.id}>
+               <img src={baseURL+val.poster_path} alt="moive poster" className="m-5"/>
+           </div>)
           })}        
     </div>
+    </>
   )
 }
 
